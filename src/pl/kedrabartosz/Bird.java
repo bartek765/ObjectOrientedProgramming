6613cdefbc1bbbbc47b3279eb8c94c43fbe1a6e8
Bartek <bartekkedra@onet.pl>
2025-01-26 00:59:53 +0100

    Bird class

---
+++ src/pl/kedrabartosz/Bird.java
@@ -0,0 +1,11 @@
+package pl.kedrabartosz;
+
+public class Bird {
+    private String name;
+    private int age;
+    private String color;
+    private double speed;
+    private double size;
+    private String breed;
+    private String eyeColor;
+}

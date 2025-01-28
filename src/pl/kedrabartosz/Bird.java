4eb00abbc1dfb08a3adc0e18db3ad5f14f81945b
Bartek <bartekkedra@onet.pl>
2025-01-28 14:32:50 +0100

    add comment

--- src/pl/kedrabartosz/Bird.java
+++ src/pl/kedrabartosz/Bird.java
@@ -8,8 +8,8 @@ public class Bird {
     private double size;
     private String breed;
     private String eyeColor;
-
-
+// pole klasy Tworzenie zmiennych w klasie, ale bez przypisania wartości.
+//konstuktor Przypisanie wartości do pól , nic nie zwraca
     public Bird(String name, int age, String color, double speed, double size, String breed, String eyeColor) {
         this.name = name;
         this.age = age;
@@ -19,11 +19,11 @@ public class Bird {
         this.breed = breed;
         this.eyeColor = eyeColor;
     }
-
+//metoda
     public void giveVoice() {
         System.out.println(this.name + " makes kwi kwi");
     }
-
+//metoda
     public void colorAnimal() {
         System.out.println("he have " + this.color + " color");
     }

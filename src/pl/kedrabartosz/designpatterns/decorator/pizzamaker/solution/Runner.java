6922bd6917b9c69a4da072728d2b741544a7f2af
Bartek <bartekkedra@onet.pl>
2025-03-09 22:27:35 +0100

    update

--- src/pl/kedrabartosz/designpatterns/decorator/pizzamaker/solution/Runner.java
+++ src/pl/kedrabartosz/designpatterns/decorator/pizzamaker/solution/Runner.java
@@ -3,7 +3,15 @@ package pl.kedrabartosz.designpatterns.decorator.pizzamaker.solution;
 public class Runner {
     public static void main(String[] args) {
         PizzaMargaritha pizzaMargaritha = new PizzaMargaritha("mozarella");
-        PizzaFunghi pizzaFunghi = new PizzaFunghi("champignons",pizzaMargaritha);
+        PizzaFunghi pizzaFunghi = new PizzaFunghi("champignons", pizzaMargaritha);
         pizzaFunghi.makePizza();
+
+        PizzaPepperoni pizzaPepperoni = new PizzaPepperoni("mozarella", "pepper", pizzaMargaritha);
+        pizzaPepperoni.makePizza();
+
+        PizzaPepperoni newpizza = new PizzaPepperoni("mozzarella", "pepper", pizzaFunghi);
+        newpizza.makePizza();
+
+
     }
 }
